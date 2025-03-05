f30205db14aaa333466b231f7fa6eef387008bab
Arvinderpal10 <arvinderpal10@gmail.com>
2025-03-05 07:28:19 +0530

    print function in Python - Session 1 March

---
+++ Day1/01.print.py
@@ -0,0 +1,31 @@
+# Hello world is the very first program that we write in any language
+
+# print("Hello World!")
+
+# print() -- print is function (system defined) -- We are just using the print() function we are not writing the code for it.
+
+# Print() -- Will not work -- Python is case-sensitive
+
+# print{} -- 
+# print(
+
+# True , true 
+
+# 2. New line after every print
+
+# Comments start with # symbols 
+
+
+# task - print -- Miruthun Python Class in one line 
+# print("Miruthun Python Class")
+# print("Miruthun") 
+# print("Python Class")
+
+
+# \n -- new line 
+
+# print("Miruthun", end=" ") 
+# print("Python Class" , end=" ")
+
+print("Miruthun", end="--") 
+print("Python Class" , end="+")
