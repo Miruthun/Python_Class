95e2d7868b51dee9a8031824707262063cf309c9
Arvinderpal10 <arvinderpal10@gmail.com>
2025-03-05 06:59:09 +0530

    Git commands

--- Hello.py
+++ Hello.py
@@ -1,1 +1,14 @@
 print("Hello World")
+
+#Collaborating for the first time -- I am going to publish something to the repository 
+
+
+# M --Modified -  We are doing changes in the existing file
+
+# U - Untracked--  This means we have created a new file/ folder
+
+# Untracked means -- Github has no record / not aware of the file /folder
+
+
+# Git status - It tells us the changes that happened oin our local copy of the code
+
