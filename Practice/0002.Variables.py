c89bcf8ee0dbff4a5909a6ffb102c0ac2308f609
Arvinderpal10 <arvinderpal10@gmail.com>
2025-03-12 07:07:52 +0530

    S2-March: Homework - variable

---
+++ Practice/0002.Variables.py
@@ -0,0 +1,99 @@
+"""
+1. Which of the following is a valid variable name in Python?
+    a) my-variable
+    b) 2ndNumber
+    c) userName_123
+    d) for
+Answer: 
+
+2. Identify the incorrect variable name:
+    a) customer_id
+    b) _privateVar
+    c) import
+    d) value123
+ Answer:
+
+3. What will happen if you use a Python keyword as a variable name?
+    a) It will throw an error
+    b) It will work fine
+    c) The variable will be ignored
+    d) It will be automatically converted into a string
+Answer:
+
+4. Which of the following is the most Pythonic way to declare a variable?
+    a) UserAge = 25
+    b) user_age = 25
+    c) userAge = 25
+    d) USERAGE = 25
+Answer:
+
+5. What is the convention for naming constants in Python?
+    a) pi_value = 3.14
+    b) PI_VALUE = 3.14
+    c) PiValue = 3.14
+    d) piValue = 3.14
+Answer:
+
+6. Which variable name is invalid in Python?
+    a) _counter
+    b) 123abc
+    c) height_in_meters
+    d) var_1
+Answer:
+
+7. Which of the following is true about variable names in Python?
+    a) They can start with a number
+    b) They can contain spaces
+    c) They are case-sensitive
+    d) They must be in lowercase only
+Answer:
+
+8. Which of the following is a valid Python variable name?
+    a) var@name
+    b) global_variable
+    c) while
+    d) 123varname
+Answer:
+
+9. Which of the following is a valid variable name in Python?
+    a) my-var
+    b) True
+    c) _data_count
+    d) 3rdNumber
+Answer:
+
+10. What will be the output of the following code?
+"""
+var1 = 10  
+Var1 = 20  
+print(var1, Var1)
+"""
+    a) 10 10       b) 20 20          c) 10 20          d) Error
+Answer:
+11. Which of the following is a convention for naming private variables in Python?
+    a) privateVar
+    b) _privateVar
+    c) PRIVATE_VAR
+    d) pvt_var
+Answer:
+
+12. What will be the output of the following code?
+"""
+variable = "Python"
+_variable = "Java"
+print(variable, _variable)
+"""
+    a) Python Java
+    b) Error
+    c) Java Python
+    d) None
+Answer:
+
+13. Which of the following variable names is invalid in Python?
+    a) user_id_1
+    b) account_balance
+    c) return
+    d) _cache_data
+Answer: 
+
+"""
