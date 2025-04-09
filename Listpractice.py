6a20a9c5e5b8dba5712219efd0b0f57ec118d635
Miruthun <Muthu.raman83@gmail.com>
2025-04-08 20:41:24 -0400

    List Practice

---
+++ Listpractice.py
@@ -0,0 +1,20 @@
+# Initializing a list
+
+list = ["milk","eggs","bread","butter","cheese"]
+
+# Add more items
+
+list.add["apples","bananas", "orange juice"]
+
+# Remove items
+
+list.remove(4,7)
+
+# Update items
+
+list(0)= "milk (cheaper brand)"
+list(2)= "bread (cheaper brand)"
+
+# Final list
+
+print(list)
