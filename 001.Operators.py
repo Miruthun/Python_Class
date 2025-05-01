3128eae1cf560d5c57e0249799b79778a2ff5d47
Arvinder Pal <54645908+Arvinderpal10@users.noreply.github.com>
2025-05-01 06:57:32 +0530

    Operators in Python

---
+++ 001.Operators.py
@@ -0,0 +1,74 @@
+# Opertors : 3+4 = 7   -- > 3,4 - Operands + --> Operator
+
+
+# Arithmetic Operators -- + , - , * ,/ ,// , ** , %
+
+# + -- Addition Operator
+print(34+23)
+
+# Subtraction Operator 
+print(34 - 23)
+
+# Multiplication operator
+print(5*23)
+
+# / -- Division Operator
+
+print(10/2)
+print(10/6)
+
+# // -- Floor Division 
+
+print(10//2)
+print(10//6)
+
+# Modulus -- Remainder Operator -- % 
+print(10%3)
+print(13%5)
+
+
+#  Exponentiation Operator 
+print(2**3) #2 *2 * 2
+
+print(5**4) #5 *5 * 5 *5 
+
+# Assignment Operators 
+
+# = 
+x = 5 # x is assigned a value 5 of type integer
+# print(x)
+
+x += 10     # x = 5 + 10
+# print(x)
+
+x -=5       # x = x - 5 
+# print(x)
+
+# x *=4
+x *=4
+# x /=5
+x /=5
+# x %=3
+x%=3
+# print(x)
+# x **=5
+x **=5
+print(x)
+
+
+# Comparision Opertors -- Always return boolean -- True / False
+
+# == -- Only used in conditionals -- We will talk about this in coming sessions 
+print(3==3)
+print(3==5)
+# less than
+print(34<56)
+# greater than 
+print(34>56)
+# less than equal to ---- <=
+print(34<=34)
+
+# greater than equal to 
+print(45>=45)
+# Not equal to 
+print(5 != 5)
