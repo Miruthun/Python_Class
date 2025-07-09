c8bd4f784a76b57edeb8b63b6609981a5cc55f75
ArvinderPal10 <arvinderpal10@gmail.com>
2025-07-09 06:51:41 +0530

    Star Patterns in Python

---
+++ Loops/StarPatterns/0001.Star.py
@@ -0,0 +1,146 @@
+# Line 
+
+# print("* * * * * ")
+# print("* "*5)
+
+# for i in range(5):
+#     print("*" , end=" ")
+
+
+# Square 
+'''
+* * * * * 
+* * * * *
+* * * * * 
+* * * * *
+* * * * *
+'''
+
+# for i in range(5):
+#     for j in range(5):
+#         print("*", end=" ")
+#     print()
+
+
+#
+"""
+* 
+* * 
+* * * 
+* * * * 
+* * * * * 
+""" 
+
+# for i in range(1, 6):
+#     for j in range(i):
+#         print("*", end=" ")
+#     print()
+
+
+"""
+* * * * *
+* * * *
+* * *
+* * 
+*
+"""
+
+# for i in range(5,0 , -1):
+#     for j in range(i):
+#         print("*", end=" ")
+#     print()
+
+"""
+A 
+B B 
+C C C
+D D D D
+E E E E E
+"""
+# counter = 65
+# for i in range(1, 6):
+    
+#     for j in range(i):
+#         print(chr(counter), end=" ")
+#     counter+=1
+#     print()
+
+"""
+1
+2 2
+3 3 3 
+4 4 4 4
+5 5 5 5 5 
+"""
+# counter =1
+# for i in range(1, 6):
+    
+#     for j in range(i):
+#         print(counter, end=" ")
+#     counter+=1
+#     print()
+
+
+# ascii values -- 
+
+"""
+A 
+A B 
+A B C
+A B C D
+A B C D E
+"""
+
+
+# for i in range(1, 6):
+#     counter = 65
+#     for j in range(i):
+#         print(chr(counter), end=" ")
+#         counter+=1
+#     print()
+
+"""
+1 
+1 2 
+1 2 3
+1 2 3 4
+1 2 3 4 5
+"""
+
+# for i in range(1, 6):
+#     counter = 1
+#     for j in range(i):
+#         print(counter, end=" ")
+#         counter+=1
+#     print()
+
+
+"""
+1
+2 1 
+3 2 1 
+4 3 2 1
+5 4 3 2 1
+"""
+
+# for i in range(1, 6):
+#     for j in range(i,0,-1):
+#         print(j, end=" ")
+#     print()
+
+
+"""
+1 
+2  3 
+4  5  6 
+7  8  9 10
+11 12 13 14 15
+
+"""
+# counter = 1
+# for i in range(1, 6):
+   
+#     for j in range(i):
+#         print(counter, end=" ")
+#         counter+=1
+#     print()
