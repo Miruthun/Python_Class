1b21614e698c5f4d65add6b024b80d80c49f196b
Miruthun <Muthu.raman83@gmail.com>
2026-01-03 11:21:50 -0500

    First Sheet Done

---
+++ LogicBuildingWorksheet1.py
@@ -0,0 +1,155 @@
+# 1.
+print("Hello World")
+
+# 2.
+for i in range(0,5):
+  print("Miruthun")
+
+# 3.
+day = "12"
+month = "December"
+year = "2025"
+print("Today is", day, month, year)
+
+# 4.
+n1 = 12
+n2 = 54
+print(n1+n2)
+
+# 5.
+print("*****")
+print("   *")
+print("  *")
+print(" *")
+print("*****")
+
+# 6.
+print("1", end = ",")
+print("2", end = ",")
+print("3", end = ",")
+print("4", end = ",")
+print("5", end = ",")
+print("6", end = ",")
+print("7", end = ",")
+print("8", end = ",")
+print("9", end = ",")
+print("10")
+
+
+# 7.
+b = 9
+if b%2 == 0:
+  print("even")
+elif b%2 == 1:
+  print("odd")
+else:
+  print("error")
+
+# 8.
+x=1
+y=1.1
+z="one"
+print(x,",",y,",",z)
+
+# 9.
+a = 14
+print("I am", a, "years old.")
+
+# 10.
+print("line 1")
+print()
+print("line 2")
+
+# 11.
+age = 14
+print("My age is", age)
+
+# 12.
+num1 = 1
+num2 = 2
+list1 = [num1,num2]
+print("The reversed order is", list1[::-1])
+
+# 13.
+Celsius = 22
+Fahrenheit = (Celsius*1.8)+32
+print(Celsius, "degrees Celsius is", Fahrenheit, "degrees in Fahrenheit.")
+
+# 14.
+user_name = input("What is your name? ")
+user_age = int(input("What is your age? "))
+print("The user's name is", user_name, "and their age is", user_age)
+
+# 15.
+length = int(input("What is the length of the rectangle? "))
+width = int(input("What is the width of the rectangle? "))
+rectangle_area = length*width
+print("The rectangle's area is", rectangle_area, "units.")
+
+# 16.
+print("Hello"+"World")
+
+# 17.
+integer_input = int(input("Give me an integer: "))
+float_input = float(input("Give me a float: "))
+integer_x_float = integer_input*float_input
+print(integer_input, "times",float_input, "equals", integer_x_float )
+
+# 18.
+Boolean = True
+print(Boolean)
+
+# 19.
+og = 56
+og = 55
+print("The new value is:", og)
+
+# 20.
+#Python doesn't allow you to leave a variable uninitialized and assign the values later. 
+#You can give it a value at the beginning and change it later, but you must assign something to it when creating it.
+
+# 21.
+user_Name = input("What is your name? ")
+print("Hello", user_Name)
+
+# 22.
+SN1 = int(input("What is number 1? "))
+SN2 = int(input("What is number 2? "))
+sum_SNs = SN1 + SN2
+print("The sum of your numbers is", sum_SNs)
+
+# 23.
+age_user = int(input("What is your age? "))
+print("Just to confirm, your age is", age_user, "right?")
+
+# 24.
+IDK = 2
+
+# 25.
+num_for_25 = int(input("Give me a number: "))
+num_sq = num_for_25**2
+print("Your number squared is", num_sq)
+
+# 26.
+user_Fname = input("What's your first name? ")
+user_Lname = input("What's your last name? ")
+print("Your full name is", user_Fname, user_Lname)
+
+# 27.
+Celsius = int(input("What's the temperature in Celsius? "))
+Fahrenheit = (Celsius*1.8)+32
+print(Celsius, "degrees Celsius is", Fahrenheit, "degrees in Fahrenheit.")
+
+# 28
+entered_num = int(input("Insert Number: "))
+print("You entered:", entered_num)
+
+# 29.
+word_1 = input("Insert Word 1: ")
+word_2 = input("Insert Word 2: ")
+print(word_1, word_2)
+
+# 30.
+inputed = input("Enter: ")
+for i in range (0,3):
+  print(inputed)
