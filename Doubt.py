01826fd3bfdbfb14d802f0a8fa4e704f9b9e1466
Arvinder Pal <arvinderpal10@gmail.com>
2026-01-17 20:37:28 +0530

    feat: Doubt.py added

---
+++ Doubt.py
@@ -0,0 +1,15 @@
+# Statically Typed  langauges and Dynamically Typed Langugage 
+
+# Python -- DTL
+
+# string , bool , int ,float 
+x = "hello"
+print(type(x))
+
+x = 30
+print(type(x))
+
+x = True
+print(type(x))
+
+
